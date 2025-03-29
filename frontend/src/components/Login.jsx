b4d4d87065b28c785ccf79ac33dd407f9d3a8327
darwme <darw.mendz@gmail.com>
2025-03-28 21:44:09 -0500

    fix endpoitn route

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     setMessage("");
     setError("");
 
-    const response = await fetch("http://127.0.0.1:8000/login/", {
+    const response = await fetch("https://tareatalleraplicaciones1.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
     setMessage("");
     setError("");
 
-    const response = await fetch("http://127.0.0.1:8000/register/", {
+    const response = await fetch("https://tareatalleraplicaciones1.onrender.com/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
